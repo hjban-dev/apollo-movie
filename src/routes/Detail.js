f15e2717bfe3aba78ccf53cbaebd8cc99b7149d4
hjban <bananagga@naver.com>
2020-02-19 15:44:46 +0900

    #1.5 Suggestions & Optional Chaining

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -13,6 +13,10 @@ const GET_MOVIE = gql`
 			rating
 			description_intro
 		}
+		suggestions(id: $id) {
+			id
+			medium_cover_image
+		}
 	}
 `;
 
@@ -58,27 +62,23 @@ const Poster = styled.div`
 export default () => {
 	let { id } = useParams();
 	id = parseInt(id);
+
 	const { loading, data } = useQuery(GET_MOVIE, {
 		variables: { id }
 	});
+	console.log(data);
+
 	return (
 		<Container>
 			<Column>
 				<Title>{loading ? "Loading..." : data.movie.title}</Title>
-				{!loading && data.movie && (
-					<>
-						<Subtitle>
-							{data.movie.language} · {data.movie.rating}
-						</Subtitle>
-						<Description>
-							{data.movie.description_intro}
-						</Description>
-					</>
-				)}
+				<Subtitle>
+					{data?.movie?.language} · {data?.movie?.rating}
+				</Subtitle>
+				<Description>{data?.movie?.description_intro}</Description>
 			</Column>
-			<Poster
-				bg={data && data.movie ? data.movie.medium_cover_image : ""}
-			></Poster>
+			<Poster bg={data?.movie?.medium_cover_image}></Poster>
+			{/* {data && data.suggestions && data.suggestions.map(s => {})} */}
 		</Container>
 	);
 };
